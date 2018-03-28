2b618c5c87d7ead3fa4dfdc98f7c904a25b54da2
Michael Grundkötter <mg@ujamii.com>
2018-03-28 22:42:16 +0200

    [TASK] added readme [FEATURE] improved configuration possibilities [FEATURE] added exception logging

--- Classes/Hooks/DataHandlerHook.php
+++ Classes/Hooks/DataHandlerHook.php
@@ -2,7 +2,10 @@
 
 namespace Ujamii\UjamiiGeocoder\Hooks;
 
+use Geocoder\Geocoder;
+use Geocoder\Provider\Provider;
 use Geocoder\Query\GeocodeQuery;
+use Http\Client\HttpClient;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
@@ -23,43 +26,48 @@ class DataHandlerHook {
 	public function processDatamap_postProcessFieldArray($status, $table, $uid, &$fieldArray, &$dataHandler) {
 		if (isset($GLOBALS['TCA'][$table]['ctrl']['geocoder'])) {
 			try {
-				$geocoderConfig = $this->checkGeocoderConfig($GLOBALS['TCA'][$table]);
+				$geocoderConfig = $this->checkGeocoderConfig( $GLOBALS['TCA'][ $table ] );
 
-				if ($status == 'new') {
+				if ( $status == 'new' ) {
 					$triggered = true;
 				} else {
-					$changedFields = array_keys($fieldArray);
-					if (count(array_intersect($changedFields, $geocoderConfig['triggerFields'])) > 0) {
+					$changedFields = array_keys( $fieldArray );
+					if ( count( array_intersect( $changedFields, $geocoderConfig['triggerFields'] ) ) > 0 ) {
 						$triggered = true;
 					} else {
 						$triggered = false;
 					}
 				}
 
-				if ($triggered) {
-					if (is_numeric($uid)) {
-						$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('*', $table, 'uid = ' . $uid);
-						$origData = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
+				if ( $triggered ) {
+					if ( is_numeric( $uid ) ) {
+						$res      = $GLOBALS['TYPO3_DB']->exec_SELECTquery( '*', $table, 'uid = ' . $uid );
+						$origData = $GLOBALS['TYPO3_DB']->sql_fetch_assoc( $res );
 					} else {
 						$origData = array();
 					}
-					$mergedData = array_merge($origData, $fieldArray);
-					$stringToGeocode = GeneralUtility::callUserFunction($geocoderConfig['getAddressString'], $mergedData, $this);
+					$mergedData      = array_merge( $origData, $fieldArray );
+					$stringToGeocode = GeneralUtility::callUserFunction( $geocoderConfig['getAddressString'], $mergedData, $this );
 
-					$httpClient = new \Http\Adapter\Guzzle6\Client();
-					$provider = new \Geocoder\Provider\GoogleMaps\GoogleMaps($httpClient);
-					$geocoder = new \Geocoder\StatefulGeocoder($provider, 'en');
+					/* @var $httpClient HttpClient */
+					$httpClient = GeneralUtility::makeInstance( $geocoderConfig['httpClientClass'], $geocoderConfig['httpClientConfig'] );
+					/* @var $provider Provider */
+					$provider = GeneralUtility::makeInstance( $geocoderConfig['providerClass'], $httpClient);
+					/* @var $geocoder Geocoder */
+					$geocoder = GeneralUtility::makeInstance( $geocoderConfig['geocoderClass'], $provider, $geocoderConfig['locale'] );
 
-					$result = $geocoder->geocodeQuery(GeocodeQuery::create($stringToGeocode));
-					if (!$result->isEmpty()) {
+					$result = $geocoder->geocodeQuery( GeocodeQuery::create( $stringToGeocode ) );
+					if ( ! $result->isEmpty() ) {
 						$firstResult = $result->first();
 
-						$fieldArray[$geocoderConfig['latField']] = $firstResult->getCoordinates()->getLatitude();
-						$fieldArray[$geocoderConfig['lngField']] = $firstResult->getCoordinates()->getLongitude();
+						$fieldArray[ $geocoderConfig['latField'] ] = $firstResult->getCoordinates()->getLatitude();
+						$fieldArray[ $geocoderConfig['lngField'] ] = $firstResult->getCoordinates()->getLongitude();
 					}
 				}
+			} catch (\Geocoder\Exception\Exception $e) {
+				GeneralUtility::devLog($e->getMessage(), 'ujamii_geocoder', GeneralUtility::SYSLOG_SEVERITY_ERROR);
 			} catch (\Exception $e) {
-				//TODO: write to log?
+				GeneralUtility::devLog($e->getMessage(), 'ujamii_geocoder', GeneralUtility::SYSLOG_SEVERITY_ERROR);
 			}
 		}
 	}
@@ -68,9 +76,20 @@ class DataHandlerHook {
 	 * @param array $tableTca The TCA array for one table.
 	 *
 	 * @return array The checked ['ctrl']['geocoder'] part of the given array.
+	 * @throws \LogicException
 	 */
 	protected function checkGeocoderConfig(array $tableTca) {
-		$config = $tableTca['ctrl']['geocoder'];
+		$defaults = [
+			'httpClientConfig' => [],
+			'locale' => 'de',
+			'latField' => 'lat',
+			'lngField' => 'lng',
+			'httpClientClass' => \Http\Adapter\Guzzle6\Client::class,
+			'providerClass' => \Geocoder\Provider\GoogleMaps\GoogleMaps::class,
+			'geocoderClass' => \Geocoder\StatefulGeocoder::class
+		];
+		$config = array_merge($defaults, $tableTca['ctrl']['geocoder']);
+
 		// there needs to be at least one field configured to trigger the geocoding process.
 		if (!isset($config['triggerFields']) || !is_array($config['triggerFields']) || empty($config['triggerFields'])) {
 			throw new \LogicException('no triggerFields set in TCA|tableName|ctrl|geocoder');
@@ -83,11 +102,22 @@ class DataHandlerHook {
 		}
 
 		// make sure there are target fields set.
-		if (!isset($config['latField'])) {
-			$config['latField'] = 'lat';
+		if (!isset($tableTca['columns'][$config['latField']])) {
+			throw new \LogicException(sprintf('latField "%s" not set in TCA|tableName|columns|fieldName', $config['latField']));
 		}
-		if (!isset($config['lngField'])) {
-			$config['lngField'] = 'lng';
+		if (!isset($tableTca['columns'][$config['lngField']])) {
+			throw new \LogicException(sprintf('lngField "%s" not set in TCA|tableName|columns|fieldName', $config['lngField']));
+		}
+
+		// verify class configuration
+		if (!is_subclass_of($config['httpClientClass'], HttpClient::class)) {
+			throw new \LogicException(sprintf('httpClientClass "%s" is not of type "%s"', $config['httpClientClass'], HttpClient::class));
+		}
+		if (!is_subclass_of($config['providerClass'], Provider::class)) {
+			throw new \LogicException(sprintf('providerClass "%s" is not of type "%s"', $config['providerClass'], Provider::class));
+		}
+		if (!is_subclass_of($config['geocoderClass'], Geocoder::class)) {
+			throw new \LogicException(sprintf('geocoderClass "%s" is not of type "%s"', $config['geocoderClass'], Geocoder::class));
 		}
 		return $config;
 	}
--- README.md
+++ README.md
@@ -1,3 +1,107 @@
 # ujamii-geocoder
 
-Connecting http://geocoder-php.org/Geocoder with TYPO3 DataHandler API
+Connecting http://geocoder-php.org/Geocoder with TYPO3 DataHandler API.
+With this extension you can easily add geo data to records while they are changed by editors in the TYPO3 backend. You just have to configure
+how your entity "looks like" in the eyes of the geocoder via TCA anf that's it.
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Example](#example)
+* [TODOs](#todos)
+
+## Installation
+
+Currently only works in composer mode of TYPO3, so
+
+```json
+"repositories": [
+    {
+      "name": "ujamii/ujamii-geocoder",
+      "type": "vcs",
+      "url": "git@github.com:ujamii/ujamii-geocoder.git"
+    }
+  ],
+```
+
+```shell
+composer require ujamii/ujamii-geocoder
+```
+
+## Usage
+
+Just add a new config array to the `ctrl` section of your TCA.
+
+```php
+..['ctrl']['geocoder'] = [
+	'triggerFields' => ['street', 'zip', 'city'],
+	'getAddressString' => 'Your\Namespace\Domain\Model\YourEntity->getAddressString'
+];
+```
+
+### Options
+
+Those options are possible:
+
+**triggerFields (mandatory)**
+Changes in those fields will trigger the process of geocoding.
+
+**getAddressString (mandatory)**
+A method called via `TYPO3\CMS\Core\Utility\GeneralUtility::callUserFunction` which is supposed
+to return a complete address string. The first parameter provided to this methid is the merged
+data array (unchanged entity data from db + changed values from the backend form).
+
+Example:
+```php
+public function getAddressString($dataArray) {
+	return $dataArray['location'];
+}
+```
+
+**locale** (default: de)
+The locale which is used in the geocoder. 
+
+**latField** (default: lat)
+Name of the target field for the latitude value.
+
+**lngField** (default: lng)
+Name of the target field for the longitude value.
+
+**httpClientClass** (default: \Http\Adapter\Guzzle6\Client::class)
+Class name of the http client, see [possible packages](https://packagist.org/providers/php-http/client-implementation)
+
+**httpClientConfig** (default: [])
+Possible options for the http client like proxy settings and so on.
+
+**providerClass** (default: \Geocoder\Provider\GoogleMaps\GoogleMaps::class)
+Class name of the provider, see [possible packages](https://packagist.org/providers/geocoder-php/provider-implementation)
+
+**geocoderClass** (default: \Geocoder\StatefulGeocoder::class)
+Class name of the geocoder.
+
+## Example
+
+Let's assume the record is a news record in the database with 3 fields: location, lat and lng. The field location
+contains something like `Alexanderplatz, Berlin, Deutschland` and lat and lng are the fields you want to be filled
+automatically as soon as an editor changes the location.
+
+Add this to your `typo3conf/ext/your_extension/Configuration/TCA/Overrides/tx_news_domain_model_news.php` 
+or `typo3conf/ext/your_extension/Configuration/TCA/tx_ext_domain_model_entity.php` file.
+
+```php
+$GLOBALS['TCA']['tx_news_domain_model_news']['ctrl']['geocoder'] = [
+	'triggerFields' => ['street', 'zip', 'city'],
+    'getAddressString' => 'Your\Namespace\Domain\Model\YourEntity->getAddressString'
+];
+```
+
+In `YourEntity`, add a method like this:
+```php
+public function getAddressString($dataArray) {
+	return sprintf('%s, %s %s', $dataArray['street'], $dataArray['zip'], $dataArray['city']);
+}
+```
+
+## TODOs
+
+* publish on packagist
+* publish in TER
